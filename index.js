80c139bdd554cb2041e4baa8f6a5af66a7dd84c5
Waheedul Islam <waheeduli81@gmail.com>
2024-09-13 03:51:51 +0600

    cart

--- index.js
+++ index.js
@@ -23,6 +23,52 @@ async function run() {
   try {
     await client.connect();
 
+    // scooter
+    const scootersCollection = client
+      .db("scooters")
+      .collection("scootersCollection");
+    // Users
+    const usersCollection = client.db("UsersDB").collection("Users");
+    // Users
+    const cartsCollection = client.db("CartsDB").collection("Carts");
+
+    ////////////////////// User Collection ////////////////////////
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          massage: "User Already Exists",
+          insertedIn: null,
+        });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    ////////////////////// Scooters Collection ////////////////////////
+    app.get("/scooters", async (req, res) => {
+      const scooterData = scootersCollection.find();
+      const result = await scooterData.toArray();
+
+      res.send(result);
+    });
+
+    ////////////////////// Carts Collection ////////////////////////
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartData = req.body;
+      const result = await cartsCollection.insertOne(cartData);
+      res.send(result);
+    });
+
     console.log("successfully connected to MongoDB!");
   } finally {
     // await client.close();
